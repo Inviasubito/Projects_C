0398f98479179effda0fb4dd7f81959eab71d529
TizioBj <Inviasubito@users.noreply.github.com>
2023-10-30 15:36:44 +0100

    Update Es6.c

--- Orienta/Es6.c
+++ Orienta/Es6.c
@@ -10,24 +10,22 @@ Scrivere un programma che riceve in input due numeri interi e ne scambia il cont
 
 int main()
 {
-    int a, b, c;
+   int a, b, c;
+   
+   printf("Inserisci A, B: ");
+   scanf("%d, %d", &a, &b);
+   
+   printf("Prima dello scambio:\n");
+   printf("A: %d \n", a);
+   printf("B: %d \n", b);
+   
+   c = a;
+   a = b;
+   b = c;
+   
+   printf("Dopo lo scambio:\n");
+   printf("A: %d \n", a);
+   printf("B: %d \n", b);
 
-    printf("A:");
-    scanf("%d", &a);
-    printf("B:");
-    scanf("%d", &b);
-
-    printf("Prima dello scambio:\n");
-    printf("A: %d \n", a);
-    printf("B: %d \n", b);
-
-    c = a;
-    a = b;
-    b = c;
-
-    printf("Dopo lo scambio:\n");
-    printf("A: %d \n", a);
-    printf("B: %d \n", b);
-
-    return 0;
+   return 0;
 }
