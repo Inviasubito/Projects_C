7229fd1523cdd7648bb9014d4c447cbe8958cea0
TizioBj <corrivai95007@live.it>
2023-10-30 15:29:49 +0100

    Es6

--- Orienta/Es6.c
+++ Orienta/Es6.c
@@ -3,12 +3,31 @@
 30/10/23
 Alessandro Di ROberto
 
-
+Scrivere un programma che riceve in input due numeri interi e ne scambia il contenuto
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-	return 0;
+    int a, b, c;
+
+    printf("A:");
+    scanf("%d", &a);
+    printf("B:");
+    scanf("%d", &b);
+
+    printf("Prima dello scambio:\n");
+    printf("A: %d \n", a);
+    printf("B: %d \n", b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("Dopo lo scambio:\n");
+    printf("A: %d \n", a);
+    printf("B: %d \n", b);
+
+    return 0;
 }
